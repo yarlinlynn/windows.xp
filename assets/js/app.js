a0d96def6b38d5a84d4be3604a28dea0a2866e39
Yarlin <yarlinlynn@gmail.com>
2026-04-26 19:55:31 +0200

    Created and styled boot up screen with animation using keyframes

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+// window.addEventListener("load", () => {
+//     const overlay = document.getElementById("black-overlay");  
+//     const loadingScreen = document.getElementById("loading-screen");
+//     const loginScreen = document.getElementById("login-screen");
+
+//     setTimeout(() => {
+//         overlay.classList.remove("active");
+//         loadingScreen.classList.add("active")
+//     }, 800);
+
+//     setTimeout(() => {
+//         loadingScreen.classList.remove("active");
+//         overlay.classList.add("active");
+//     }, 3000);
+
+//     setTimeout(() => {
+//         overlay.classList.remove("active");
+//         loginScreen.classList.add("active")
+//     }, 3800);
+
+// });
